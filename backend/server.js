ad0cd36ff883e4b562f133d4c222807e712e9723
raja-krishnanrrg <b.rajakrishnan017@gmail.com>
2026-04-24 13:20:51 +0530

    some changesss

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config(); // 🔥 THIS MUST BE BEFORE EVERYTHING
 
+
+import path from "path"
+
 import taskRoutes from "./routes/taskRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js"
@@ -26,10 +29,15 @@ mongoose.connect(process.env.MONGO_URI)
     console.log("❌ DB Error:", err.message);
   });
 
-// Test route
-// app.get("/", (req, res) => {
-//   res.send("API Running...");
-// });
+const __dirname =path.resolve();
+if(process.env.NODE_ENV === "production"){
+  const frontendpath=path.join(__dirname,"..","frontend","dist")
+  app.use(express.static(frontendpath))
+  app.use('*',(req,res)=>{
+    res.sendFile(path.join(frontendpath,'index.html'))
+  })
+}   
+
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/admin", adminRoutes);
