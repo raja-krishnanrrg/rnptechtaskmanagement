0331cbcc291fe0e7aca322f27995b132b2114f56
raja-krishnanrrg <b.rajakrishnan017@gmail.com>
2026-04-24 17:39:33 +0530

    url is from rnpserver

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config(); // 🔥 THIS MUST BE BEFORE EVERYTHING
-
-
-// import path from "path"
-
-// import taskRoutes from "./routes/taskRoutes.js";
-// import authRoutes from "./routes/authRoutes.js";
-// import adminRoutes from "./routes/adminRoutes.js"
-
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-
-//  // .env load
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // 🔥 MongoDB Connect
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("✅ MongoDB Connected");
-//   })
-//   .catch((err) => {
-//     console.log("❌ DB Error:", err.message);
-//   });
-
-// const __dirname =path.resolve();
-// if(process.env.NODE_ENV === "production"){
-//   const frontendpath=path.join(__dirname,"..","frontend","dist")
-//   app.use(express.static(frontendpath))
-//   app.use('*',(req,res)=>{
-//     res.sendFile(path.join(frontendpath,'index.html'))
-//   })
-// }   
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/tasks", taskRoutes);
-// app.use("/api/admin", adminRoutes);
-
-// // Server start
-// app.listen(5000, () => {
-//   console.log("🚀 Server running on port 5000");
-// });
-
 
 
 import dotenv from "dotenv";
@@ -64,6 +15,11 @@ import adminRoutes from "./routes/adminRoutes.js";
 const app = express();
 
 // Middleware
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
 app.use(cors());
 app.use(express.json());
 
